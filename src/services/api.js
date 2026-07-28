75993b48883b88f1581478e466f5ff76017ba03a
diorizqi404 <rizqiii.dio@gmail.com>
2026-07-28 21:33:00 +0700

    change api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://3000.devops.my.id/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'https://api.eepis.web.id/api/v1',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
